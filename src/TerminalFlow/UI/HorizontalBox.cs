c45866f2c48a4c1a6ad75f3dd1b0a2ed78b17dff
Akihisa Yagi <capra314cabra@gmail.com>
2019-12-31 17:21:55 +0900

    Implement HorizontalBox

---
+++ src/TerminalFlow/UI/HorizontalBox.cs
@@ -0,0 +1,74 @@
+using System;
+using System.Collections.Generic;
+
+using TerminalFlow.Core;
+
+namespace TerminalFlow
+{
+    public class HorizontalBox : ConsoleUI
+    {
+        private List<ConsoleUI> m_UIs =
+            new List<ConsoleUI>();
+
+        public override ConsoleSize Size => m_Size;
+        private ConsoleSize m_Size;
+
+        public HorizontalBox(params ConsoleUI[] children)
+        {
+            foreach(var child in children)
+            {
+                Add(child);
+            }
+        }
+
+        public void Add(ConsoleUI ui)
+        {
+            m_UIs.Add(ui);
+            ui.OnRepaint += OnReceiveRepaintEvent;
+            ui.OnResize += OnReceiveResizeEvent;
+
+            UpdateSize();
+        }
+
+        private void UpdateSize()
+        {
+            int maxHeight = 0;
+            int width = 0;
+
+            foreach(var ui in m_UIs)
+            {
+                maxHeight = Math.Max(maxHeight, ui.Size.Height);
+                width += ui.Size.Width;
+            }
+
+            m_Size = new ConsoleSize(width, maxHeight);
+        }
+
+        public override void Display()
+        {
+            var currentPos = ConsoleVec2.Zero;
+            var startPos = ConsoleVec2.Current;
+
+            foreach(var ui in m_UIs)
+            {
+                var absolutePos = startPos + currentPos;
+                absolutePos.Move();
+
+                ui.Display();
+
+                currentPos.X += ui.Size.Width;
+            }
+        }
+
+        private void OnReceiveRepaintEvent(ConsoleUI ui)
+        {
+            InvokeRepaint();
+        }
+
+        private void OnReceiveResizeEvent(ConsoleUI ui)
+        {
+            UpdateSize();
+            InvokeResize();
+        }
+    }
+}
