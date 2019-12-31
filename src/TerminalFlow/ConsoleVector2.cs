cebefc07cd346238dedc2f9b5be2ccef00de01e0
Akihisa Yagi <capra314cabra@gmail.com>
2019-12-31 16:55:50 +0900

    Add Zero field

--- src/TerminalFlow/ConsoleVector2.cs
+++ src/TerminalFlow/ConsoleVector2.cs
@@ -2,10 +2,13 @@ using System;
 
 namespace TerminalFlow
 {
-    public struct ConsoleVec2
+    public class ConsoleVec2
     {
         public int X { get; set; }
         public int Y { get; set; }
+
+        public static ConsoleVec2 Zero =>
+            new ConsoleVec2(0, 0);
 
         public ConsoleVec2(int x, int y)
         {
@@ -20,5 +23,10 @@ namespace TerminalFlow
 
         public static ConsoleVec2 Current =>
             new ConsoleVec2(Console.CursorLeft, Console.CursorTop);
+
+        public static ConsoleVec2 operator+(ConsoleVec2 a, ConsoleVec2 b)
+        {
+            return new ConsoleVec2(a.X + b.X, a.Y + b.Y);
+        }
     }
 }
